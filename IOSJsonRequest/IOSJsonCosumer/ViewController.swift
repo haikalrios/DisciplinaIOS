805b72c680790bca9e313a465251c27155e8db89
Haikal Rios <haikalrios@gmail.com>
2017-10-05 18:42:30 -0700

    mapa

--- IOSJsonRequest/IOSJsonCosumer/ViewController.swift
+++ IOSJsonRequest/IOSJsonCosumer/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!{
+        didSet{
+            mapView.delegate =  self
+            let longPress = UILongPressGestureRecognizer(target: self, action: #selector(addPin(_:)))
+            longPress.minimumPressDuration = 2
+            mapView.addGestureRecognizer(longPress)
+        }
     }
+    
+    @objc private func addPin(_ sender: UILongPressGestureRecognizer){
+        let annotations  = mapView.annotations.filter{ (annotation) -> Bool in
+            return annotation.isKind(of: MKPointAnnotation.self)
+        }
+        mapView.removeAnnotations(annotations)
+        
+        let touchPoint = sender.location(in: mapView)
+        let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "haikal"
+        annotation.subtitle = "oi"
+        
+        mapView.addAnnotation(annotation)
+    }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
     }
 
 
 }
+
+extension ViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation.isKind(of: MKPointAnnotation.self){
+            let view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "myMarker")
+            view.isDraggable = true
+            return view
+        }
+       return nil
+    }
+ 
+}
+
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+        if let location = locations.last{
+            let camera = MKMapCamera(lookingAtCenter: location.coordinate, fromDistance: 2000, pitch: 0, heading: 0)
+            mapView.setCamera(camera, animated: true)
+            mapView.showsUserLocation =  true
+        }
+    }
+}
+
+
 
